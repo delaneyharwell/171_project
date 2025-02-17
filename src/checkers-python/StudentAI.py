19d198eb6fcfc798172f030ce10cb84688010c66
neeraja <neerajakulkar23@gmail.com>
2025-02-16 20:43:33 -0800

    only StudentAI modified

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -1,5 +1,6 @@
-from random import randint
+from random import randint, choice
 from BoardClasses import Move
+import copy
 from BoardClasses import Board
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
@@ -14,14 +15,36 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
+
+    def simulate_random_game(self, board, player):
+        temp_board = copy.deepcopy(board)
+        current_player = player
+        while not temp_board.is_win(self.color) and not temp_board.is_win(self.opponent[self.color]):
+            possible_moves = temp_board.get_all_possible_moves(current_player)
+            if not possible_moves:
+                break
+            move = choice(choice(possible_moves))
+            temp_board.make_move(move, current_player)
+            current_player = self.opponent[current_player]
+        return 1 if temp_board.is_win(self.color) else 0
+
     def get_move(self,move):
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
         else:
             self.color = 1
         moves = self.board.get_all_possible_moves(self.color)
-        index = randint(0,len(moves)-1)
-        inner_index =  randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]
-        self.board.make_move(move,self.color)
-        return move
+        if not moves:
+            return None
+        best_move = None
+        best_score = -1
+        for move_list in moves:
+            for move in move_list:
+                temp_board = copy.deepcopy(self.board)
+                temp_board.make_move(move, self.color)
+                wins = sum(self.simulate_random_game(temp_board, self.opponent[self.color]) for _ in range(10))
+                if wins > best_score:
+                    best_score = wins
+                    best_move = move
+        self.board.make_move(best_move,self.color)
+        return best_move
